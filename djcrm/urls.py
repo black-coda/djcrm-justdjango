9732c53b53b1aa9992b9bcc710f34384be2f4be8
monday solomon <monday_solomon01@gmail.com>
2021-10-15 10:18:14 +0100

    Changes

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += [
-        static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
-    ]
+#if settings.DEBUG:
+ #   urlpatterns += [
+  #      static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+   # ]
--- leads/models.py
+++ leads/models.py
@@ -22,3 +22,4 @@ class Lead(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
